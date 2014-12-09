081873a875847a268e140403d01bf8e4fceae466
Michal Klempa <mkl@eea.sk>
2014-12-09 13:23:11 +0100

    Last modified date

---
+++ l-filesToLocalFS/src/main/java/eu/unifiedviews/plugins/loader/filestolocalfs/ConvertUtils.java
@@ -0,0 +1,30 @@
+package eu.unifiedviews.plugins.loader.filestolocalfs;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class ConvertUtils {
+    private static final String DATE_FORMAT = "yyyy-MM-dd'T'HH:mm:ss.SSS000";
+
+    public static String dateToString(Date date) {
+        DateFormat df = new SimpleDateFormat(DATE_FORMAT);
+        if (date != null) {
+            return df.format(date);
+        } else {
+            return null;
+        }
+    }
+
+    public static Date stringToDate(String strDate) throws ParseException {
+        DateFormat df = new SimpleDateFormat(DATE_FORMAT);
+        Date result = null;
+        if (!StringUtils.isBlank(strDate)) {
+            result = df.parse(strDate);
+        }
+        return result;
+    }
+}
--- l-filesToLocalFS/src/main/java/eu/unifiedviews/plugins/loader/filestolocalfs/FilesToLocalFS.java
+++ l-filesToLocalFS/src/main/java/eu/unifiedviews/plugins/loader/filestolocalfs/FilesToLocalFS.java
@@ -9,14 +9,10 @@ import java.nio.file.StandardCopyOption;
 import java.nio.file.attribute.PosixFilePermissions;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
-import org.openrdf.model.Resource;
-import org.openrdf.model.ValueFactory;
-import org.openrdf.model.impl.URIImpl;
-import org.openrdf.model.vocabulary.DCTERMS;
-import org.openrdf.model.vocabulary.RDF;
-import org.openrdf.repository.RepositoryConnection;
-import org.openrdf.repository.RepositoryException;
+import org.apache.commons.io.FilenameUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -24,7 +20,6 @@ import com.sun.nio.file.ExtendedCopyOption;
 
 import eu.unifiedviews.dataunit.DataUnit;
 import eu.unifiedviews.dataunit.DataUnitException;
-import eu.unifiedviews.dataunit.MetadataDataUnit;
 import eu.unifiedviews.dataunit.files.FilesDataUnit;
 import eu.unifiedviews.dataunit.files.WritableFilesDataUnit;
 import eu.unifiedviews.dpu.DPU;
@@ -32,6 +27,7 @@ import eu.unifiedviews.dpu.DPUContext;
 import eu.unifiedviews.dpu.DPUException;
 import eu.unifiedviews.helpers.dataunit.copyhelper.CopyHelper;
 import eu.unifiedviews.helpers.dataunit.copyhelper.CopyHelpers;
+import eu.unifiedviews.helpers.dataunit.resourcehelper.ResourceHelpers;
 import eu.unifiedviews.helpers.dataunit.virtualpathhelper.VirtualPathHelper;
 import eu.unifiedviews.helpers.dataunit.virtualpathhelper.VirtualPathHelpers;
 import eu.unifiedviews.helpers.dpu.config.AbstractConfigDialog;
@@ -73,8 +69,7 @@ public class FilesToLocalFS extends
         }
         File destinationDirFile = new File(config.getDestination());
         destinationDirFile.mkdirs();
-        String destinationAbsolutePath = destinationDirFile
-                .getAbsolutePath();
+        String destinationAbsolutePath = destinationDirFile.getAbsolutePath();
 
         boolean moveFiles = config.isMoveFiles();
         ArrayList<CopyOption> copyOptions = new ArrayList<>(1);
@@ -88,10 +83,7 @@ public class FilesToLocalFS extends
         boolean shouldContinue = !dpuContext.canceled();
         VirtualPathHelper inputVirtualPathHelper = VirtualPathHelpers.create(filesInput);
         CopyHelper copyHelper = CopyHelpers.create(filesInput, filesOutput);
-        RepositoryConnection outputMetadataConnection = null;
         try {
-            outputMetadataConnection = filesOutput.getConnection();
-            ValueFactory valueFactory = outputMetadataConnection.getValueFactory();
             while (shouldContinue && filesIteration.hasNext()) {
                 index++;
 
@@ -106,6 +98,7 @@ public class FilesToLocalFS extends
                     }
                     File outputFile = new File(destinationAbsolutePath + File.separator
                             + outputRelativePath);
+                    new File(FilenameUtils.getFullPath(outputFile.getAbsolutePath())).mkdirs();
 
                     Path outputPath = outputFile.toPath();
 
@@ -119,28 +112,19 @@ public class FilesToLocalFS extends
                         java.nio.file.Files.copy(inputPath, outputPath, copyOptionsArray);
                     }
                     java.nio.file.Files.setPosixFilePermissions(outputPath, PosixFilePermissions.fromString("rw-r--r--"));
+
                     copyHelper.copyMetadata(entry.getSymbolicName());
-
-                    Resource symbolicNameResource = outputMetadataConnection.getStatements(
-                            null, valueFactory.createURI(MetadataDataUnit.PREDICATE_SYMBOLIC_NAME), valueFactory.createLiteral(entry.getSymbolicName()),
-                            false, filesOutput.getMetadataGraphnames().toArray(new URIImpl[0])).next().getSubject();
-
-                    outputMetadataConnection.add(valueFactory.createStatement(
-                            symbolicNameResource, RDF.TYPE, DCAT.Distribution),
-                            filesOutput.getMetadataWriteGraphname());
-
-                    outputMetadataConnection.add(valueFactory.createStatement(
-                            symbolicNameResource, DCTERMS.MODIFIED, valueFactory.createLiteral(new Date())),
-                            filesOutput.getMetadataWriteGraphname());
-
-                    outputMetadataConnection.add(valueFactory.createStatement(
-                            symbolicNameResource, DCAT.accessURL, valueFactory.createURI(outputFile.toURI().toASCIIString())),
-                            filesOutput.getMetadataWriteGraphname());
+                    Map<String, String> resource = ResourceHelpers.getResource(filesOutput, entry.getSymbolicName());
+                    if (resource == null) {
+                        resource = new HashMap<>();
+                    }
+                    resource.put("last_modified", ConvertUtils.dateToString(new Date()));
+                    ResourceHelpers.setResource(filesOutput, entry.getSymbolicName(), resource);
 
                     if (dpuContext.isDebugging()) {
                         LOG.debug("Processed {} file in {}s", appendNumber(index), (System.currentTimeMillis() - start.getTime()) / 1000);
                     }
-                } catch (IOException | RepositoryException ex) {
+                } catch (IOException ex) {
                     if (config.isSkipOnError()) {
                         LOG.warn("Error processing {} file {}", appendNumber(index), String.valueOf(entry), ex);
                     } else {
@@ -153,13 +137,6 @@ public class FilesToLocalFS extends
         } catch (DataUnitException ex) {
             throw new DPUException("Error iterating filesInput.", ex);
         } finally {
-            if (outputMetadataConnection != null) {
-                try {
-                    outputMetadataConnection.close();
-                } catch (RepositoryException ex) {
-                    LOG.warn("Error in close", ex);
-                }
-            }
             try {
                 filesIteration.close();
             } catch (DataUnitException ex) {
