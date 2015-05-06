36e4cd8c19023e7d6394374cb98f7d7aadcaf3c6
mbl-eea-sk <michal.bielik@eea.sk>
2015-05-06 08:26:36 +0200

    Initial stack projektu

---
+++ t-gunzipper/src/main/java/eu/unifiedviews/plugins/transformer/gunzipper/GunzipperConfig_V1.java
@@ -0,0 +1,14 @@
+package eu.unifiedviews.plugins.transformer.gunzipper;
+
+/**
+ * Configuration class for FilesMerger.
+ *
+ * @author Petr Škoda
+ */
+public class GunzipperConfig_V1 {
+
+    public GunzipperConfig_V1() {
+
+    }
+
+}
---
+++ t-gunzipper/src/main/java/eu/unifiedviews/plugins/transformer/gunzipper/GunzipperVaadinDialog.java
@@ -0,0 +1,36 @@
+package eu.unifiedviews.plugins.transformer.gunzipper;
+
+import com.vaadin.ui.Label;
+import com.vaadin.ui.VerticalLayout;
+
+import eu.unifiedviews.dpu.config.DPUConfigException;
+import eu.unifiedviews.helpers.dpu.vaadin.dialog.AbstractDialog;
+
+/**
+ * Vaadin configuration dialog for FilesMerger.
+ *
+ * @author Petr Škoda
+ */
+public class GunzipperVaadinDialog extends AbstractDialog<GunzipperConfig_V1> {
+
+    public GunzipperVaadinDialog() {
+        super(Gunzipper.class);
+    }
+
+    @Override
+    public void setConfiguration(GunzipperConfig_V1 c) throws DPUConfigException {
+
+    }
+
+    @Override
+    public GunzipperConfig_V1 getConfiguration() throws DPUConfigException {
+        final GunzipperConfig_V1 c = new GunzipperConfig_V1();
+
+        return c;
+    }
+
+    @Override
+    public void buildDialogLayout() {
+    }
+
+}
