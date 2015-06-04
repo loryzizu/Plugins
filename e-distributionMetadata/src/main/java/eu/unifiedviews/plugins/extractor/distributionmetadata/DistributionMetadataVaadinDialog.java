558c937451be8ce0578d10b7b737de578ccfe44f
Michal Klempa <mkl@eea.sk>
2015-06-04 14:22:55 +0200

    Draft of validation form

--- e-distributionMetadata/src/main/java/eu/unifiedviews/plugins/extractor/distributionmetadata/DistributionMetadataVaadinDialog.java
+++ e-distributionMetadata/src/main/java/eu/unifiedviews/plugins/extractor/distributionmetadata/DistributionMetadataVaadinDialog.java
@@ -4,46 +4,20 @@ import java.lang.reflect.Field;
 
 import com.vaadin.data.fieldgroup.BeanFieldGroup;
 import com.vaadin.data.fieldgroup.FieldGroup.CommitException;
+import com.vaadin.ui.AbstractComponent;
 import com.vaadin.ui.AbstractTextField;
 import com.vaadin.ui.Component;
-import com.vaadin.ui.DateField;
-import com.vaadin.ui.TextField;
 import com.vaadin.ui.VerticalLayout;
 
 import eu.unifiedviews.dpu.config.DPUConfigException;
 import eu.unifiedviews.helpers.dpu.vaadin.dialog.AbstractDialog;
+import eu.unifiedviews.helpers.dpu.vaadin.validator.UrlValidator;
 
 public class DistributionMetadataVaadinDialog extends AbstractDialog<DistributionMetadataConfig_V1> {
 
     private static final long serialVersionUID = 7003725620084616056L;
 
-    private DistributionMetadataConfig_V1 config = null; //new DistributionMetadataConfig_V1();
-
     BeanFieldGroup<DistributionMetadataConfig_V1> binder;
-
-    private TextField dcatAccessURL = new TextField();
-
-    private TextField dctermsDescription = new TextField();
-
-    private TextField dctermsFormat = new TextField();
-
-    private TextField dctermsLicense = new TextField();
-
-    private TextField dcatDownloadURL = new TextField();
-
-    private TextField dcatMediaType = new TextField();
-
-    private DateField dctermsIssued = new DateField();
-
-    private TextField dctermsTitle = new TextField();
-
-    private TextField wdrsDescribedBy = new TextField();
-
-    private TextField podDistributionDescribedByType = new TextField();
-
-    private TextField dctermsTemporal = new TextField();
-
-    private TextField voidExampleResource = new TextField();
 
     public DistributionMetadataVaadinDialog() {
         super(DistributionMetadata.class);
@@ -57,49 +31,45 @@ public class DistributionMetadataVaadinDialog extends AbstractDialog<Distributio
         mainLayout.setWidth("100%");
         mainLayout.setHeight(null);
         mainLayout.setMargin(false);
-        //mainLayout.setSpacing(true);
+//        mainLayout.setSpacing(true);
 
         // top-level component properties
         setWidth("100%");
         setHeight("100%");
-        for (Field f : this.getClass().getDeclaredFields()) {
-            if (Component.class.isAssignableFrom(f.getType())) {
-                try {
-                    ((Component) f.get(this)).setCaption(ctx.tr(this.getClass().getSimpleName() + "." + f.getName() + ".caption"));
-                    ((Component) f.get(this)).setSizeFull();
-                    if (AbstractTextField.class.isAssignableFrom(f.getType())) {
-                        ((AbstractTextField) f.get(this)).setInputPrompt(ctx.tr(this.getClass().getSimpleName() + "." + f.getName() + ".inputPrompt"));
-                        ((AbstractTextField) f.get(this)).setNullRepresentation("");
-                    }
-                    mainLayout.addComponent((Component) f.get(this));
-                } catch (IllegalArgumentException | IllegalAccessException ex) {
-                    ex.printStackTrace();
-                }
+        binder = new BeanFieldGroup<DistributionMetadataConfig_V1>(DistributionMetadataConfig_V1.class);
+        DistributionMetadataConfig_V1 config = new DistributionMetadataConfig_V1();
+        binder.setItemDataSource(config);
+        for (Field f : DistributionMetadataConfig_V1.class.getDeclaredFields()) {
+            Component component = null;
+            component = binder.buildAndBind(ctx.tr(this.getClass().getSimpleName() + "." + f.getName() + ".caption"), f.getName());
+            component.setSizeFull();
+            if (AbstractTextField.class.isAssignableFrom(component.getClass())) {
+                ((AbstractTextField) component).setInputPrompt(ctx.tr(this.getClass().getSimpleName() + "." + f.getName() + ".inputPrompt"));
+                ((AbstractTextField) component).setNullRepresentation("");
+                ((AbstractTextField) component).addValidator(new UrlValidator());
             }
+            if (AbstractComponent.class.isAssignableFrom(component.getClass())) {
+                ((AbstractComponent) component).setImmediate(true);
+            }
+            
+            mainLayout.addComponent(component);
         }
-
         setCompositionRoot(mainLayout);
     }
 
     @Override
     public void setConfiguration(DistributionMetadataConfig_V1 conf) throws DPUConfigException {
-        if (config == null) {
-            config = conf;
-            binder = new BeanFieldGroup<DistributionMetadataConfig_V1>(DistributionMetadataConfig_V1.class);
-            binder.setItemDataSource(config);
-            binder.buildAndBindMemberFields(this);
-        }
+        binder.setItemDataSource(conf);
     }
 
-    @SuppressWarnings("unchecked")
     @Override
     public DistributionMetadataConfig_V1 getConfiguration() throws DPUConfigException {
         try {
             binder.commit();
         } catch (CommitException ex) {
-            throw new DPUConfigException(ex);
+            throw new DPUConfigException(ctx.tr(this.getClass().getSimpleName() + ".validation.exception"));
         }
-        return config;
+        return binder.getItemDataSource().getBean();
     }
 
 }
