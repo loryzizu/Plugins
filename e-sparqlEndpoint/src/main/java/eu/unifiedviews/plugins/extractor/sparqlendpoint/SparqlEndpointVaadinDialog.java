f418e7316c58baa6eb65825f6eef652f7daf4b82
Michal Klempa <mkl@eea.sk>
2015-08-26 09:33:52 +0200

    Revert "Reverting branch resolve#241 - chunked mode - more cleanups - test resources, typos, imports"
    This reverts commit b9d95d3d9eeb9a35ff12b6c8e0c26bff39c24768.

--- e-sparqlEndpoint/src/main/java/eu/unifiedviews/plugins/extractor/sparqlendpoint/SparqlEndpointVaadinDialog.java
+++ e-sparqlEndpoint/src/main/java/eu/unifiedviews/plugins/extractor/sparqlendpoint/SparqlEndpointVaadinDialog.java
@@ -5,6 +5,8 @@ import org.openrdf.query.QueryLanguage;
 import org.openrdf.query.parser.QueryParserUtil;
 
 import com.vaadin.data.Validator;
+import com.vaadin.ui.AbstractSelect.NewItemHandler;
+import com.vaadin.ui.ComboBox;
 import com.vaadin.ui.TextArea;
 import com.vaadin.ui.TextField;
 import com.vaadin.ui.VerticalLayout;
