caa11cc04e621550c9cd6f5049a98306b016d0b2
eea03 <tomas.caban@eea.sk>
2015-12-29 20:31:35 +0100

    New DPU for executing HTTP requests enabling consuming REST and SOAP web services

---
+++ e-httpRequest/src/main/java/eu/unifiedviews/plugins/extractor/httprequest/HttpRequestHelper.java
@@ -0,0 +1,34 @@
+package eu.unifiedviews.plugins.extractor.httprequest;
+
+import java.io.IOException;
+
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class HttpRequestHelper {
+
+    private static final Logger LOG = LoggerFactory.getLogger(HttpRequestHelper.class);
+
+    public static void tryCloseHttpClient(CloseableHttpClient client) {
+        if (client != null) {
+            try {
+                client.close();
+            } catch (IOException e) {
+                LOG.warn("Failed to close HTTP client", e);
+            }
+        }
+    }
+
+    public static void tryCloseHttpResponse(CloseableHttpResponse response) {
+        if (response != null) {
+            try {
+                response.close();
+            } catch (IOException e) {
+                LOG.warn("Failed to close HTTP response", e);
+            }
+        }
+    }
+
+}
